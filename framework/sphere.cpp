17af0ad2320fc76b7c917c5f2139fc4381a7451d
yorrickslr <yoyo.sieler@gmail.com>
2015-06-27 23:46:50 +0200

    Add method definitions to sphere

--- framework/sphere.cpp
+++ framework/sphere.cpp
@@ -1,2 +1,34 @@
 #include "sphere.hpp"
+#include <glm/vec3.hpp>
 #include <cmath>
+
+Sphere::Sphere(glm::vec3 center, double radius) :
+	center_{center},
+	radius_{radius}
+{}
+
+Sphere::Sphere() :
+	center_{0.0,0.0,0.0},
+	radius_{1}
+{}
+
+Sphere::~Sphere() {}
+
+double Sphere::volume() const {
+	return (12.56636 * std::pow(radius_,3)) / 3 ;
+}
+
+double Sphere::area() const {
+	return 12.56636 * radius_ * radius_;
+}
+
+glm::vec3 Sphere::center() const {
+	return center_;
+}
+
+double Sphere::radius() const {
+	return radius_;
+}
+
+// MinGW does not support M_PI, 
+// so I replaced it with 3.14159 and reduced the equations
