bfbd6d68e594bf59735b85b234f9439e3eeca713
yorrickslr <yoyo.sieler@gmail.com>
2015-06-28 00:35:56 +0200

    Add declarations for box

---
+++ framework/box.hpp
@@ -0,0 +1,21 @@
+#ifndef BUW_BOX_HPP
+#define BUW_BOX_HPP
+
+#include "shape.hpp"
+#include <glm/vec3.hpp>
+
+class Box : public Shape {
+public:
+	Box();
+	Box(glm::vec3 const& min, glm::vec3 const& max);
+	~Box();
+	glm::vec3 min() const;
+	glm::vec3 max() const;
+	double area() const override;
+	double volume() const override;
+private:
+	glm::vec3 min_;
+	glm::vec3 max_;
+};
+
+#endif
