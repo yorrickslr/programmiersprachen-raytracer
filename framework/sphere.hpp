7520c6ccecd8d474b04e208dd328d7f6de9119db
yorrickslr <yoyo.sieler@gmail.com>
2015-06-28 00:30:23 +0200

    Fix const correctness

--- framework/sphere.hpp
+++ framework/sphere.hpp
@@ -1,15 +1,21 @@
 #ifndef BUW_SPHERE_HPP
 #define BUW_SPHERE_HPP
-#include <glm/vec3.hpp>
+
 #include "shape.hpp"
+#include <glm/vec3.hpp>
 
 class Sphere : public Shape {
 public:
-	double area() override;
-	double volume() override;
+	Sphere(glm::vec3 const& center, double const radius);
+	Sphere();
+	~Sphere();
+	glm::vec3 center() const;
+	double radius() const;
+	double area() const override;
+	double volume() const override;
 private:
-	glm::vec3 center;
-	double radius;
+	glm::vec3 center_;
+	double radius_;
 };
 
 #endif
