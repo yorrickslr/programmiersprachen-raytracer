be9c1635e5021ec52a2ff87c9b256ce30bc15563
yorrickslr <yoyo.sieler@gmail.com>
2015-06-28 02:58:29 +0200

    Add shape attributes to sphere.cpp

--- framework/sphere.cpp
+++ framework/sphere.cpp
@@ -2,17 +2,22 @@
 #include <glm/vec3.hpp>
 #include <cmath>
 
+Sphere::Sphere(std::string const& name, glm::vec3 const& center, double const radius, Color const& color) :
+	Shape{name, color},
+	center_{center},
+	radius_{radius}
+{}
+
 Sphere::Sphere(glm::vec3 const& center, double const radius) :
 	center_{center},
 	radius_{radius}
 {}
 
 Sphere::Sphere() :
+	Shape{},
 	center_{0.0,0.0,0.0},
 	radius_{1}
 {}
-
-Sphere::~Sphere() {}
 
 double Sphere::volume() const {
 	return (12.56636 * std::pow(radius_,3)) / 3 ;
