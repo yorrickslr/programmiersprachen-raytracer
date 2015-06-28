f34b09583aaca4a637b8ee9ace23ac9765640753
yorrickslr <yoyo.sieler@gmail.com>
2015-06-28 20:34:28 +0200

    Add ostream

--- framework/sphere.hpp
+++ framework/sphere.hpp
@@ -15,9 +15,12 @@ public:
 	double radius() const;
 	double area() const override;
 	double volume() const override;
+	std::ostream& print(std::ostream& os) const override;
 private:
 	glm::vec3 center_;
 	double radius_;
 };
+
+std::ostream& operator<<(std::ostream& os, Sphere const& s);
 
 #endif
