0248667a8d03eb04591eade1c397fc1f181ca9eb
yorrickslr <yoyo.sieler@gmail.com>
2015-06-28 20:35:05 +0200

    Add ostream

--- framework/shape.hpp
+++ framework/shape.hpp
@@ -10,11 +10,14 @@ public:
 	Shape(std::string const& name, Color const& color);
 	virtual double area() const = 0;
 	virtual double volume() const = 0;
-	virtual std::string name() const;
-	virtual Color color() const;
+	std::string name() const;
+	Color color() const;
+	virtual std::ostream& print(std::ostream& os) const;
 private:
 	Color color_;
 	std::string name_;
 };
+
+std::ostream& operator<<(std::ostream& os, Shape const& s);
 
 #endif
