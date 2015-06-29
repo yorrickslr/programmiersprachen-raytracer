2313871f2f76b3d29f132d4b3cbe7f1d76f3802a
yorrickslr <yoyo.sieler@gmail.com>
2015-06-29 17:34:07 +0200

    Finalize

--- framework/box.cpp
+++ framework/box.cpp
@@ -1,11 +1,16 @@
 #include "box.hpp"
 #include <glm/vec3.hpp>
+#include <iostream>
+#include <glm/glm.hpp>
+#include <algorithm>
 
 Box::Box() :
 	Shape{},
 	min_{0,0,0},
 	max_{1,1,1}
-{}
+{
+	std::cout << "Construction of box" << std::endl;
+}
 
 Box::Box(glm::vec3 const& min, glm::vec3 const& max) :
 	Shape{},
@@ -19,7 +24,9 @@ Box::Box(glm::vec3 const& min, glm::vec3 const& max) :
 		max.y>min.y ? max.y : min.y,
 		max.z>min.z ? max.z : min.z
 	}
-{}
+{
+	std::cout << "Construction of box" << std::endl;
+}
 
 Box::Box(glm::vec3 const& min, glm::vec3 const& max, Color const& color, std::string const& name) :
 	Shape{color,name},
@@ -33,7 +40,13 @@ Box::Box(glm::vec3 const& min, glm::vec3 const& max, Color const& color, std::st
 		max.y>min.y ? max.y : min.y,
 		max.z>min.z ? max.z : min.z
 	}
-{}
+{
+	std::cout << "Construction of box" << std::endl;
+}
+
+Box::~Box() {
+	std::cout << "Destruction of box" << std::endl;
+}
 
 glm::vec3 Box::min() const {
 	return min_;
@@ -66,4 +79,31 @@ std::ostream& Box::print(std::ostream& os) const {
 
 std::ostream& operator<<(std::ostream& os, Box const& s) {
 	return s.print(os);
+}
+
+bool Box::intersect(Ray const& ray, float& distance) {
+	float tx1 {(min_.x - ray.origin.x) * glm::normalize(ray.direction).x};
+	float tx2 {(max_.x - ray.origin.x) * glm::normalize(ray.direction).x};
+	float tmin {std::min(tx1,tx2)};
+	float tmax {std::max(tx1,tx2)};
+	float w {tmax - tmin};
+
+	float ty1 {(min_.y - ray.origin.y) * glm::normalize(ray.direction).y};
+	float ty2 {(max_.y - ray.origin.y) * glm::normalize(ray.direction).y};
+	float tmp {abs(ty1 - ty2)};
+	float h {tmp!=tmp ? 0.0 : tmax - tmin};
+
+	float tz1 {(min_.z - ray.origin.z) * glm::normalize(ray.direction).z};
+	float tz2 {(max_.z - ray.origin.z) * glm::normalize(ray.direction).z};
+	tmin = std::max(tmin, std::min(tz1,tz2));
+	tmax = std::min(tmax, std::max(tz1,tz2));
+	tmp = abs(tz1 - tz2);
+	float l {tmp!=tmp ? 0.0 : tmax - tmin};
+
+	if(tmax >= std::max(0.0f, tmin)) {
+		distance = sqrt(w * w + h * h + l * l);
+		return true;
+	} else {
+		return false;
+	}
 }
