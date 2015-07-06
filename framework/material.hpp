34a9356f475e4fa245f94159f64c0f8330de1648
DreadNoize <Joachim.Billert@uni-weimar.de>
2015-07-06 19:37:07 +0200

    material class
    added material class

---
+++ framework/material.hpp
@@ -0,0 +1,29 @@
+#ifndef BUW_MATERIAL_HPP
+#define BUW_MATERIAL_HPP
+#include <string>
+#include <color.hpp>
+
+class Material {
+public:
+	Material();
+	Material(std::string const& name; Color ka; Color kd; Color ks; float m);
+	~Material();
+
+	std::string get_name() const;
+	Color get_ka() const;
+	Color get_kd() const;
+	Color get_ks() const;
+	float get_m() const;
+
+
+private:
+	std::string name_;
+	Color ka_;
+	Color kd_;
+	Color ks_;
+	float m_;
+};
+
+std::ostream& operator<<(std::ostream& os, Material const& m);
+
+#endif
