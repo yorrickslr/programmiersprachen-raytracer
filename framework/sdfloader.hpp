bb94a2347eac86a2cec3cee8a96ebfaef355fcbd
yorrickslr <yoyo.sieler@gmail.com>
2015-07-07 22:02:42 +0200

    sdf parser split implement

--- framework/sdfloader.hpp
+++ framework/sdfloader.hpp
@@ -5,11 +5,12 @@
 #include <scene.hpp>
 #include <iostream>
 #include <fstream>
+#include <vector>
 
 void loadScene(std::ifstream& file, Scene& scene) {
 	std::string line{""};
 	while(std::getline(file,line)) {
-		std::cout << line << std::endl;
+		vector<std::string> input = split(line," ");
 	}
 }
 
