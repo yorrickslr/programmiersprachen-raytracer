77f922384c7d7241afeb2bd887229f5b44c53806
DreadNoize <Joachim.Billert@uni-weimar.de>
2015-07-07 19:27:40 +0200

    fixed typos

--- framework/light.cpp
+++ framework/light.cpp
@@ -1,7 +1,7 @@
 #include <light.hpp>
 
 Light::Light() :
-	light_position_{ glm::vec{0.0,0.0,0.0} },
+	light_position_{ glm::vec3{0.0,0.0,0.0} },
 	intensity_{ Color{0.0,0.0,0.0} } {}
 
 Light::Light(glm::vec3 pos) : 
@@ -9,7 +9,7 @@ Light::Light(glm::vec3 pos) :
 	intensity_{ Color{0.0,0.0,0.0} } {}
 
 Light::Light(Color intensity) :
-	light_position_{ glm::vec{0.0,0.0,0.0} },
+	light_position_{ glm::vec3{0.0,0.0,0.0} },
 	intensity_{ intensity } {}
 
 Light::Light(glm::vec3 pos, Color intensity) :
