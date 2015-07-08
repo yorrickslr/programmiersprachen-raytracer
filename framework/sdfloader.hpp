636acb9d242294d79c5412cde4e1367a57e443da
yorrickslr <yoyo.sieler@gmail.com>
2015-07-08 14:56:48 +0200

    Replace hardcoded test

--- framework/sdfloader.hpp
+++ framework/sdfloader.hpp
@@ -22,7 +22,18 @@ bool sdf_isMaterial(std::string const& input, Material*& material) {
 	if(std::regex_match(input,rgx_material)) {
 		std::vector<std::string> parsed;
 		sdf_splitString(input,parsed);
-		material = new Material{parsed[2],{1,1,1},{1,1,1},{1,1,1},4.2};
+		Color ka = {std::stof(parsed[3]),std::stof(parsed[4]),std::stof(parsed[5])};
+		Color kd = {std::stof(parsed[6]),std::stof(parsed[7]),std::stof(parsed[8])};
+		Color ks = {std::stof(parsed[9]),std::stof(parsed[10]),std::stof(parsed[11])};
+		material = new Material{parsed[2],ka,kd,ks,std::stof(parsed[12])};
+		return true;
+	}
+	return false;
+}
+
+bool sdf_isComment(std::string const& input) {
+	std::regex rgx_comment{"^#.*"};
+	if(std::regex_match(input, rgx_comment)) {
 		return true;
 	}
 	return false;
@@ -31,12 +42,16 @@ bool sdf_isMaterial(std::string const& input, Material*& material) {
 void sdf_loadScene(std::ifstream& file, Scene& scene) {
 	std::string line{""};
 	while(std::getline(file,line)) {
+		//Comment
+		if(sdf_isComment(line)) {
+			continue;
+		}
 		// Material
 		Material* tmp_material{nullptr};
 		if(sdf_isMaterial(line,tmp_material)) {
 			scene.materials.push_back(tmp_material);
+			continue;
 		}
-		tmp_material = nullptr;
 	}
 }
 
