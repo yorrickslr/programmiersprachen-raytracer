5374cf850059b838b2e395c0b01c9c6e5f9b1bd4
DreadNoize <Joachim.Billert@uni-weimar.de>
2015-07-28 15:29:56 +0200

    Ok, I’m ultra stupid!

--- framework/camera.cpp
+++ framework/camera.cpp
@@ -7,14 +7,14 @@ Camera::Camera():
 	fov_x{0.0},
 	up{{0.0,0.0,0.0}} {}
 
-Camera::Camera(glm::vec3 pos) :
+Camera::Camera(glm::vec3 const& pos) :
 	name{"Camera_Obscura"},
 	eye{ pos },
 	direction{{0,-1,0}},
 	fov_x{0.0},
 	up{{0.0,0.0,0.0}} {}
 
-Camera::Camera(String const& name, glm::vec3 const& pos, glm::vec3 const& dir, float& fov_x, glm::vec3 const& up) : 
+Camera::Camera(std::string const& name, glm::vec3 const& pos, glm::vec3 const& dir, float& fov_x, glm::vec3 const& up) : 
 	name{ name },
 	eye{ pos },
 	direction{ dir },
@@ -26,28 +26,28 @@ Camera::Camera(Camera const& cam) :
 	eye{ cam.eye },
 	direction{ cam.direction },
 	fov_x{ cam.fov_x },
-	up{{ cam.up }{}
+	up{ cam.up }{}
 
 Camera::~Camera(){}
 
 
-String get_name() const {
+std::string Camera::get_name() const {
 	return name;
 }
 
-glm::vec3 get_position() const {
+glm::vec3 Camera::get_position() const {
 	return eye;
 }
 
-glm::vec3 get_direction() const {
+glm::vec3 Camera::get_direction() const {
 	return direction;
 }
 
-float get_fov_x() const {
+float Camera::get_fov_x() const {
 	return fov_x;
 }
 
-glm::vec3 get_up() const {
+glm::vec3 Camera::get_up() const {
 	return up;
 }
 
--- framework/camera.hpp
+++ framework/camera.hpp
@@ -7,18 +7,18 @@ class Camera{
 public:
 	Camera();
 	Camera(glm::vec3 const& pos);
-	Camera(String const& name, glm::vec3 const& eye, glm::vec3 const& direction, float& fov_x, glm::vec3 const& up);
+	Camera(std::string const& name, glm::vec3 const& eye, glm::vec3 const& direction, float& fov_x, glm::vec3 const& up);
 	Camera(Camera const& cam);
 	~Camera();
 
-	String get_name() const;
+	std::string get_name() const;
 	glm::vec3 get_position() const;
 	glm::vec3 get_direction() const;
 	float get_fov_x() const;
 	glm::vec3 get_up() const;
 
 private:
-	String name;
+	std::string name;
 	glm::vec3 eye;
 	glm::vec3 direction;
 	float fov_x;
