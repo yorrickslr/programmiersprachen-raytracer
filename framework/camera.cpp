947bfc7b80a955bae6ce743e9646648a787a929b
DreadNoize <Joachim.Billert@uni-weimar.de>
2015-07-28 15:24:28 +0200

    some sweet new constructors and members

--- framework/camera.cpp
+++ framework/camera.cpp
@@ -1,14 +1,53 @@
 #include <camera.hpp>
 
 Camera::Camera():
-	cam_position_{ glm::vec3{0.0,0.0,0.0} }{}
+	name{"Camera_Obscura"},
+	eye{ glm::vec3{0.0,0.0,0.0} },
+	direction{{0,-1,0}},
+	fov_x{0.0},
+	up{{0.0,0.0,0.0}} {}
 
 Camera::Camera(glm::vec3 pos) :
-	cam_position_{ pos }{}
+	name{"Camera_Obscura"},
+	eye{ pos },
+	direction{{0,-1,0}},
+	fov_x{0.0},
+	up{{0.0,0.0,0.0}} {}
+
+Camera::Camera(String const& name, glm::vec3 const& pos, glm::vec3 const& dir, float& fov_x, glm::vec3 const& up) : 
+	name{ name },
+	eye{ pos },
+	direction{ dir },
+	fov_x{ fov_x },
+	up{ up }{}
+
+Camera::Camera(Camera const& cam) : 
+	name{ cam.name },
+	eye{ cam.eye },
+	direction{ cam.direction },
+	fov_x{ cam.fov_x },
+	up{{ cam.up }{}
 
 Camera::~Camera(){}
 
 
-glm::vec3 Camera::get_position() const {
-	return cam_position_;
+String get_name() const {
+	return name;
 }
+
+glm::vec3 get_position() const {
+	return eye;
+}
+
+glm::vec3 get_direction() const {
+	return direction;
+}
+
+float get_fov_x() const {
+	return fov_x;
+}
+
+glm::vec3 get_up() const {
+	return up;
+}
+
--- framework/camera.hpp
+++ framework/camera.hpp
@@ -1,17 +1,29 @@
 #ifndef BUW_CAMERA_HPP
 #define BUW_CAMERA_HPP
 #include <glm/vec3.hpp>
+#include <string>
 
 class Camera{
 public:
 	Camera();
-	Camera(glm::vec3 pos);
+	Camera(glm::vec3 const& pos);
+	Camera(String const& name, glm::vec3 const& eye, glm::vec3 const& direction, float& fov_x, glm::vec3 const& up);
+	Camera(Camera const& cam);
 	~Camera();
 
+	String get_name() const;
 	glm::vec3 get_position() const;
+	glm::vec3 get_direction() const;
+	float get_fov_x() const;
+	glm::vec3 get_up() const;
 
 private:
-	glm::vec3 cam_position_;
+	String name;
+	glm::vec3 eye;
+	glm::vec3 direction;
+	float fov_x;
+	glm::vec3 up;
+
 };
 
 #endif
