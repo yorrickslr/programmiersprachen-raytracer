f61d87ac9b34ee5c71312dc815c009ed86a74331
DreadNoize <Joachim.Billert@uni-weimar.de>
2015-08-20 19:15:12 +0200

    should parse shapes, but no solution for composite

--- framework/sdfloader.cpp
+++ framework/sdfloader.cpp
@@ -26,16 +26,62 @@ Scene& nsdf_loadScene(std::ifstream& file) {
     if(line.empty() || input[0].at(0)=='#') {
       std::cout << lineCount << ": empty line detected, nothing to do...." << std::endl;
     } else if(input[0]=="define" && input[1]=="shape") {
-      if(input[2]=="box") {
+
+      if(input[2]=="box" && input.size() == 10) {
         std::cout << lineCount << ": box detected, going to parse..." << std::endl;
-        std::shared_ptr<Shape> ptr = std::make_shared<Box>(Box{});
+
+        // Box components
+        glm::vec3 min{std::stod(input[4]),std::stod(input[5]),std::stod(input[6])};
+        glm::vec3 max{std::stod(input[7]),std::stod(input[8]),std::stod(input[9])};
+        std::string name = input[3];
+        auto iterator = materials.find(input[10]);
+
+        if(iterator == materials.end()) {
+          throw std::logic_error("No Material found. Cannot parse line " + lineCountStr);
+        }
+
+        // Make smart pointer to box and add it to composite
+        std::shared_ptr<Shape> ptr = std::make_shared<Box>(min, max, iterator->second, name);
         scene->composite->add(ptr); //shitty code, muss aber irgendwie? =S
-      } else if(input[2]=="sphere") {
+
+
+      } else if(input[2]=="sphere" && input.size() == 8) {
         std::cout << lineCount << ": sphere detected, going to parse..." << std::endl;
+
+        // Sphere components
+        glm::vec3 center{std::stod(input[4]), std::stod(input[5]), std::stod(input[6])};
+        auto iterator = materials.find(input[8]);
+
+        if(iterator == materials.end()) {
+          throw std::logic_error("No Material found. Cannot parse line " + lineCountStr);
+        }
+
+        // Make smart pointer to Sphere and add it to composite  
+        std::shared_ptr<Shape> ptr = std::make_shared<Sphere>(center, std::stod(input[7]), iterator->second, input[3]);
+        scene->composite->add(ptr);
+        
       } else if(input[2]=="triangle") {
         std::cout << lineCount << ": triangle detected, going to parse..." << std::endl;
+
+        // Triangle components
+        glm::vec3 p1{std::stod(input[4]),std::stod(input[5]),std::stod(input[6])};
+        glm::vec3 p2{std::stod(input[7]),std::stod(input[8]),std::stod(input[9])};
+        glm::vec3 p3{std::stod(input[10]),std::stod(input[11]),std::stod(input[12])};
+        auto iterator = materials.find(input[13]);
+
+        if(iterator == materials.end()) {
+          throw std::logic_error("No Material found. Cannot parse line " + lineCountStr);
+        }
+
+        // Make smart pointer to Triangle and add it to composite
+        std::shared_ptr<Shape> ptr = std::make_shared<Triangle>(input[3], p1, p2, p3, iterator->second);
+        scene->composite->add(ptr);
+
       } else if(input[2]=="composite") {
         std::cout << lineCount << ": composite detected, going to parse..." << std::endl;
+        std::shared_ptr<Shape> ptr = std::make_shared<Composite>(input[3]);
+        scene->composite->add(ptr);
+
       } else {
         throw std::logic_error("cannot parse shape at line " + lineCountStr);
       }
